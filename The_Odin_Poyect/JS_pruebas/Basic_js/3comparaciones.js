039ac2756deed2f20176798b52323543d43d0704
Erolayer_CL <ibanezignacio2005@gmail.com>
2026-09-15 23:41:12 -0300

    Cursos

---
+++ The_Odin_Poyect/JS_pruebas/Basic_js/3comparaciones.js
@@ -0,0 +1,14 @@
+let a= "0";
+console.log(a==true);
+console.log(a==false);  
+
+let shoppingDone = false;
+let childAllowance;
+
+if (shoppingDone === true) {
+  childAllowance = 10;
+} else {
+  childAllowance = 5;
+}
+//&& AND
+//|| OR
---
+++ The_Odin_Poyect/JS_pruebas/Basic_js/variables.js
@@ -0,0 +1,7 @@
+let ca = 12;
+const cb = 15;
+const pi = 3.14;
+let nose=undefined;
+let mouth=null;
+console.log(typeof ca);
+console.log("Hola " + cb);
